ba9efdf2aa076ca96cfc51c6276d9a9df5c0babe
Masha Baidachna <baidachnasm@gmail.com>
2025-10-21 18:00:05 -1000

    Added a version with entanglement encoding

--- utils/angle_encoding_script.py
+++ utils/angle_encoding_script.py
@@ -22,18 +22,18 @@ num_qubits = 64 # data.shape[1]**2
 dev = qml.device("default.qubit", wires=num_qubits)
 
 @qml.qnode(dev)
-def angle_circuit(phi):
+def patch_angle_circuit(phi):
     for j in range(4):
         qml.RX(np.pi * phi[j], wires=j)
     return [qml.expval(qml.PauliZ(j)) for j in range(4)]
 
-def patch_angle_encoding(data, sample=0):
+def angle_encoding(data, sample=0):
     new_dim = data.shape[1] // 2
 
     out = np.zeros((new_dim, new_dim, 4))
     for i in range(0, data.shape[1], 2):
         for j in range(0, data.shape[1], 2):
-            q_results = angle_circuit(
+            q_results = patch_angle_circuit(
                 [
                     data[sample, i, j],
                     data[sample, i, j+1],
@@ -44,6 +44,25 @@ def patch_angle_encoding(data, sample=0):
             for c in range(4):
                 out[i//2, j//2, c] = q_results[c]
     return out
+
+# Fast, more expressive angle encoding
+@qml.qnode(dev)
+def entangled_angle_circuit(phi):
+    # Encode four pixel intensities
+    for j in range(4):
+        qml.RX(np.pi * phi[j], wires=j)
+    
+    # Add local entanglement
+    qml.CNOT(wires=[0, 1])
+    qml.CNOT(wires=[2, 3])
+    qml.CNOT(wires=[1, 2])
+    
+    # # Add a small random rotation layer
+    # for j in range(4):
+    #     qml.RZ(0.3 * np.pi * np.random.rand(), wires=j)
+    
+    return [qml.expval(qml.PauliZ(j)) for j in range(4)]
+
 
 # %%
 # encoded_data = [angle_encoding(data_X, sample) for sample in range(num_samples)]
